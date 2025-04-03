68747cb0bbd903c2313a2e5bd9f2dddf14f47b16
Watanabe Tsubasa (渡邊 翼) <watanabe-tsuba@aeonpeople.biz>
2025-04-03 15:16:00 +0900

    first-commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/_index";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Welcome />;
+}
---
+++ app/routes/test.tsx
@@ -0,0 +1,19 @@
+import type { Route } from "./+types/_index";
+import { useLoaderData } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export const loader = () => {
+  console.log("test");
+  return {data: "test"};
+}
+
+export default function Home() {
+  const {data} = useLoaderData<typeof loader>();
+  return <>{data}</>;
+}
